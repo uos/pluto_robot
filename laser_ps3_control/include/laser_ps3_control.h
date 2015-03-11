12f8559a33bdfc143eb7409b49b28b4b910c2c77
robot11 <robot11@blabla>
2015-03-11 10:41:19 +0100

    Now pluto is runnable.

---
+++ laser_ps3_control/include/laser_ps3_control.h
@@ -0,0 +1,97 @@
+
+/*
+ *
+ * Copyright (C) 2015 University of Osnabrück, Germany
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * uos_diffdrive_teleop_ps3.cpp
+ *
+ *  Created on: 05.03.2015
+ *      Author: Sebastian Pütz <spuetz@uos.de>
+ */
+
+#ifndef _LASER_PS3_CONTROL_
+#define _LASER_PS3_CONTROL_
+
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <sensor_msgs/Joy.h>
+#include <algorithm>
+
+#include <actionlib/client/simple_action_client.h>
+#include <actionlib/client/terminal_state.h>
+#include <rotunit_snapshotter/Scan360Action.h>
+
+#define PS3_BUTTON_SELECT            0
+#define PS3_BUTTON_STICK_LEFT        1
+#define PS3_BUTTON_STICK_RIGHT       2
+#define PS3_BUTTON_START             3
+#define PS3_BUTTON_CROSS_UP          4
+#define PS3_BUTTON_CROSS_RIGHT       5
+#define PS3_BUTTON_CROSS_DOWN        6
+#define PS3_BUTTON_CROSS_LEFT        7
+#define PS3_BUTTON_REAR_LEFT_2       8
+#define PS3_BUTTON_REAR_RIGHT_2      9
+#define PS3_BUTTON_REAR_LEFT_1       10
+#define PS3_BUTTON_REAR_RIGHT_1      11
+#define PS3_BUTTON_ACTION_TRIANGLE   12
+#define PS3_BUTTON_ACTION_CIRCLE     13
+#define PS3_BUTTON_ACTION_CROSS      14
+#define PS3_BUTTON_ACTION_SQUARE     15
+#define PS3_BUTTON_PAIRING           16
+
+#define PS3_AXIS_STICK_LEFT_LEFTWARDS    0
+#define PS3_AXIS_STICK_LEFT_UPWARDS      1
+#define PS3_AXIS_STICK_RIGHT_LEFTWARDS   2
+#define PS3_AXIS_STICK_RIGHT_UPWARDS     3
+#define PS3_AXIS_BUTTON_CROSS_UP         4
+#define PS3_AXIS_BUTTON_CROSS_RIGHT      5
+#define PS3_AXIS_BUTTON_CROSS_DOWN       6
+#define PS3_AXIS_BUTTON_CROSS_LEFT       7
+#define PS3_AXIS_BUTTON_REAR_LEFT_2      8
+#define PS3_AXIS_BUTTON_REAR_RIGHT_2     9
+#define PS3_AXIS_BUTTON_REAR_LEFT_1      10
+#define PS3_AXIS_BUTTON_REAR_RIGHT_1     11
+#define PS3_AXIS_BUTTON_ACTION_TRIANGLE  12
+#define PS3_AXIS_BUTTON_ACTION_CIRCLE    13
+#define PS3_AXIS_BUTTON_ACTION_CROSS     14
+#define PS3_AXIS_BUTTON_ACTION_SQUARE    15
+#define PS3_AXIS_ACCELEROMETER_LEFT      16
+#define PS3_AXIS_ACCELEROMETER_FORWARD   17
+#define PS3_AXIS_ACCELEROMETER_UP        18
+#define PS3_AXIS_GYRO_YAW                19
+
+class LaserPS3Control{
+
+  public:
+    LaserPS3Control(ros::NodeHandle &nh);
+    ~LaserPS3Control();
+
+  private:
+    void PS3Callback(const sensor_msgs::Joy::ConstPtr &joy);
+
+    ros::NodeHandle nh_;
+    ros::Subscriber joy_sub_;
+    ros::Publisher vel_pub_;
+    
+    double acc_;
+    double vel_;
+    double scan_vel_;
+    actionlib::SimpleActionClient<rotunit_snapshotter::Scan360Action> ac;
+};
+
+
+#endif /* laser_ps3_control.h */
---
+++ laser_ps3_control/src/laser_ps3_control.cpp
@@ -0,0 +1,81 @@
+#include "laser_ps3_control.h"
+
+LaserPS3Control::LaserPS3Control(ros::NodeHandle &nh)
+  : nh_(nh),
+    ac("scan360", true),
+    vel_(0),
+    scan_vel_(0.6)
+{
+
+  ROS_INFO("Waiting for snapshotter360 action server to start.");
+  ac.waitForServer(); 
+
+  ROS_INFO("Connected to server.");
+  nh_.param("acc", acc_, 0.01);           // max velocity
+  vel_pub_ = nh_.advertise<geometry_msgs::Twist>("rot_vel", 1);
+  joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 15,  &LaserPS3Control::PS3Callback, this);
+
+}
+
+LaserPS3Control::~LaserPS3Control(){
+
+}
+
+void LaserPS3Control::PS3Callback(const sensor_msgs::Joy::ConstPtr &joy){
+  geometry_msgs::Twist msg;
+  if(joy->buttons[PS3_BUTTON_REAR_RIGHT_2])
+    vel_ += acc_;
+  if(joy->buttons[PS3_BUTTON_REAR_RIGHT_1])
+    vel_ -= acc_;
+  
+  if(vel_ > 1.5) vel_ = 1.5;
+
+  msg.angular.z = vel_;
+  vel_pub_.publish(msg);
+  
+  if(joy->buttons[PS3_BUTTON_ACTION_CIRCLE]){
+    int ret;
+    ret = system("source ~/ros/muffin-dry/setup.sh");
+    ROS_INFO("System call \"source\" returned %d", ret);
+    ros::Duration(1.0).sleep();
+  }
+
+  rotunit_snapshotter::Scan360Goal goal;
+  if(joy->buttons[PS3_BUTTON_ACTION_TRIANGLE]){
+    msg.angular.z = scan_vel_;
+    vel_pub_.publish(msg);
+    ros::Duration(5.0).sleep();
+    ROS_INFO("start scanning.");
+    goal.number_rounds = 1;
+    // send a goal to the action
+    ac.sendGoal(goal);
+    bool finished_before_timeout =
+      ac.waitForResult(ros::Duration(30.0));
+
+    if (finished_before_timeout)
+    {
+      actionlib::SimpleClientGoalState state =
+        ac.getState();
+      ROS_INFO("Action finished:%s",state.toString().c_str());
+    }
+    else
+      ROS_INFO("Action did not finish before the time out.");
+ 
+    msg.angular.z = 0;
+    vel_pub_.publish(msg);
+
+  }
+}
+
+int main(int args, char**argv){
+  ros::init(args, argv, "laser_ps3_control");
+  
+  ros::NodeHandle nh;
+  
+  LaserPS3Control laser_ps3_crontrol(nh);
+
+  while(ros::ok){
+    ros::spinOnce();
+  }
+  return EXIT_SUCCESS;
+}
